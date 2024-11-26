4456bb0593408e8770345984468326bbd5b14afe
SKReddySasi <skreddysasi776@gmail.com>
2024-11-26 15:13:09 +0530

    exploring get, post and delete

--- src/app.js
+++ src/app.js
@@ -1,19 +1,36 @@
+// Import the express library to create the server
 const express = require("express");
 
+// Create an Express app instance
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from the server!");
+// Handling GET requests to "/user" route
+app.get("/user", (req, res) => {
+  // Sending a JSON response with first name and last name on a GET request
+  res.send({ firstName: "Sasi", lastName: "Reddy" });
 });
 
-app.get("/test", (req, res) => {
-  res.send("Hello from the test!!!!! 54!!!");
+// Handling POST requests to "/user" route
+app.post("/user", (req, res) => {
+  // Assuming saving data to the database.
+  // In a real application, you would interact with a database here
+  res.send("Data successfully saved to the database");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("Hello from the hello!!!!! 54!!!");
+// Handling DELETE requests to "/user" route
+app.delete("/user", (req, res) => {
+  // Responding with a success message after deleting user data.
+  res.send("Deleted successfully");
 });
 
+// Middleware to handle requests starting with "/user"
+// "app.use" applies to all HTTP methods (GET, POST, DELETE, etc.) for the "/user" route
+app.use("/user", (req, res) => {
+  // Sending a JSON response with first name and last name for any request to "/user"
+  res.send({ firstName: "Sasi", lastName: "Kumar" });
+});
+
+// Starting the server and listening on port 7777
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
 });
